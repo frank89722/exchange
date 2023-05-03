ac8203a6526948c0bd129c3e1a3400fcd41d0e2d
frank89722 <mail@frankv.me>
2023-05-04 01:17:13 +0800

    enable virtual thread handler for tomcat

--- core/src/main/java/me/frankv/core/config/ApplicationConfig.java
+++ core/src/main/java/me/frankv/core/config/ApplicationConfig.java
@@ -1,10 +1,12 @@
 package me.frankv.core.config;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Clock;
 import java.time.ZoneId;
+import java.util.concurrent.Executors;
 
 @Configuration
 public class ApplicationConfig {
@@ -12,5 +14,10 @@ public class ApplicationConfig {
     @Bean(name = "clock")
     public Clock applicationTimeZoneClock() {
         return Clock.system(ZoneId.of("Asia/Taipei"));
+    }
+
+    @Bean
+    public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+        return protocolHandler -> protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
     }
 }
